7c910b951618bbe5f5ebe5e18aaab343aef4ac8b
hecka_Jm7 <77819603+Jm7hecka@users.noreply.github.com>
2023-06-30 19:45:39 +0100

    Update neural_network.py
    - another hidden layer added
    - model can be saved/ loaded
    - works like framework now

--- hand write digital recognising/neural_network.py
+++ hand write digital recognising/neural_network.py
@@ -1,40 +1,51 @@
 import numpy as np
-from keras.datasets import mnist #include dataset 
+from keras.models import save_model
 from matplotlib import pyplot 
+import csv 
 
-def sigmoid(x): #activative function
-    x = np.clip(x, -500, 500)
-    return 1 / (1 + np.exp(-x))
+class Activation_function():
+    def sigmoid(self, x): #output in range(0, 1)
+        x = np.clip(x, -500, 500)
+        return 1 / (1 + np.exp(-x))
 
-def sigmoid_derivative(x): #find the rate of change of sigmoid function
-    return x * (1-x)       #finding derivation is limit of difference quotient, which is f(x+h) - f(x) / h
-                           #in other words it is dy/dx, if you know maths you will know the m of gradient = dy/dx
-                           #this definition will be useful later
+    def sigmoid_derivative(self, x): #find the rate of change of sigmoid function
+        return x * (1-x)       #finding derivation is limit of difference quotient, which is f(x+h) - f(x) / h
+                            #in other words it is dy/dx, if you know maths you will know the m of gradient = dy/dx
+                            #this definition will be useful later
 
-def relu(x): #activation function
-    return np.maximum(0, x)
+    def relu(self, x): #turn negative value into 0
+        return np.maximum(0, x)
 
-def relu_derivative(x): #find the rate of change of relu function
-    return (x > 0).astype(float)
+    def relu_derivative(self, x): #find the rate of change of relu function
+        return (x > 0).astype(float)
 
-def mean_squared_error(x, y):
-    return np.mean((x - y) **2) 
 
-class Convulational_neural_network():
-    def __init__(self, train_data, train_label, test_data, test_label, learning_rate):
+class MLP():
+    def __init__(self):
+        self.train_X = None
+        self.train_y = None
+        self.test_X = None
+        self.test_y = None
+        self.input = None
+        self.hidden = None
+        self.output = None
+        self.weight_1 = None
+        self.weight_2 = None
+        self.weight_3 = None
+        self.bias_1 = None
+        self.bias_2 = None
+        self.bias_3 = None
+        self.function = Activation_function()
+
+    def import_data(self, train_data, train_label, test_data, test_label):
         self.train_X = train_data.reshape(-1, 28*28)/ 255 #turn all train image into 1D array
         self.imgdata = test_data
         self.test_X = test_data.reshape(-1, 28*28) / 255 #turn all test image into 1D array
         self.train_y = train_label
         self.test_y = test_label
-        self.input = 28*28 #define input neuron 
-        self.hidden = 256 #define neuron in hidden layer
-        self.output = 10 #define output neuron(1-10)
-        self.learning_rate = learning_rate 
-        self.weight_1 = 0.02 * np.random.random((self.input, self.hidden)) - 0.01 #define weight for input layer
-        self.weight_2 = 0.2 * np.random.random((self.hidden, self.output)) - 0.1 #defile weight for hidden layer
-        self.bias_1 = np.zeros((1, self.hidden)) #define bias for input layer  
-        self.bias_2 = np.zeros((1, self.output)) #define bias for hidden layer
+
+    def mean_squared_error(self, x, y):
+         return np.mean((x - y) **2) 
 
     def transformLabels(self, labels):     #turn label into one-hot data
         zeroArray = [0,0,0,0,0,0,0,0,0,0]  #for example 1 = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -53,63 +64,132 @@ class Convulational_neural_network():
                 max = array[i]
         return num
     
-   
     def forward(self, x): #forward propagation, find output from input value
-        self.output = []
-        self.layer_1 = relu(np.dot(x, self.weight_1) + self.bias_1) #hidden layer process
-        self.output = sigmoid(np.dot(self.layer_1, self.weight_2 + self.bias_2)) #output layer process
+        self.output = None
+        self.layer_1 = self.function.relu(np.dot(x, self.weight_1) + self.bias_1) #hidden layer 1 process
+        self.layer_2 = self.function.relu(np.dot(self.layer_1, self.weight_2) + self.bias_2) #hidden layer 2 process
+        self.output = self.function.sigmoid(np.dot(self.layer_2, self.weight_3 + self.bias_3)) #output layer process
     
-    def backward(self, x, y): #changing weight and bias value base on error rate 
+    def backpropagation(self, x, y): #changing weight and bias value base on error rate 
         x = x.reshape(-1, len(x))
-        error_2 = y - self.output #find error rate of layer 2 by doing subtraction between actual output and predicted output
-        gradient_decent_2 = error_2 * sigmoid_derivative(self.output) #finding gradient decent(mx) of layer 2
+        error_3 = y - self.output #find error rate of output layer by doing subtraction between actual output and predicted output
+        gradient_decent_3 = error_3 * self.function.sigmoid_derivative(self.output) #finding gradient decent(mx) of output layer
+        error_2 = gradient_decent_3.dot(self.weight_3.T) #find error rate of hidden layer 2
+        gradient_decent_2 = error_2 * self.function.relu_derivative(self.layer_2) #find gradient decent of hidden layer 2
         error_1 = gradient_decent_2.dot(self.weight_2.T) #find error rate of layer 1
-        gradient_decent_1 = error_1 * relu_derivative(self.layer_1) #find gradient decent of layer 1
-        self.weight_2 += self.layer_1.T.dot(gradient_decent_2) * self.learning_rate #update weight value of layer 2
+        gradient_decent_1 = error_1 * self.function.relu_derivative(self.layer_1) #find gradient decent of layer 1
+        self.weight_3 += self.layer_2.T.dot(gradient_decent_3) * self.learning_rate #update weight value of output layer
+        self.weight_2 += self.layer_1.T.dot(gradient_decent_2) * self.learning_rate #update weight value of output layer
         self.weight_1 += x.T.dot(gradient_decent_1) * self.learning_rate #update weight value of layer 1
-        self.bias_2 += np.sum(gradient_decent_2, axis=0) * self.learning_rate #update bias value of layer 2
-        self.bias_1 += np.sum(gradient_decent_1, axis=0) * self.learning_rate #update bias value of layer 1 
+        self.bias_3 += np.sum(gradient_decent_3, axis=0) * self.learning_rate #update bias value of output layer 
+        self.bias_2 += np.sum(gradient_decent_2, axis=0) * self.learning_rate #update bias value of hidden layer 2
+        self.bias_1 += np.sum(gradient_decent_1, axis=0) * self.learning_rate #update bias value of layer 1  
 
-    def train(self, epochs): 
+    def train(self, input_neuron, hidden_neuron, output_neuron, learning_rate, epochs): 
         print('Training data...')
+        if self.train_X.all() == None:
+            print("ERROR: Dataset not found")
+            return 0
+        self.input = input_neuron #define input neuron 
+        self.hidden = list(hidden_neuron) #define neuron in hidden layer
+        self.output = output_neuron #define output neuron(1-10)
+        self.learning_rate = learning_rate 
+        self.weight_1 = 0.002 * np.random.random((self.input, self.hidden[0])) - 0.001 #define weight for input layer
+        self.weight_2 = 0.02 * np.random.random((self.hidden[0], self.hidden[1])) - 0.01 #define weight for hidden layer 1
+        self.weight_3 = 0.2 * np.random.random((self.hidden[1], self.output)) - 0.1 #defile weight for hidden layer 2
+        self.bias_1 = np.zeros((1, self.hidden[0])) #define bias for input layer  
+        self.bias_2 = np.zeros((1, self.hidden[1])) #define bias for hidden layer 1  
+        self.bias_3 = np.zeros((1, self.output)) #define bias for hidden layer 2
         y = self.transformLabels(self.train_y) #transform all train label into one-hot value
-        for i in range(epochs): #epochs represents how many times for all train data will be tested
+        for i in range(epochs): #epochs represents how many times train data will be tested
             for j in range(len(self.train_X)):
                 self.forward(self.train_X[j])
-                self.backward(self.train_X[j], y[j])
+                self.backpropagation(self.train_X[j], y[j])
             if i % 1== 0: #find loss for each 100 epochs
-                print(f"Epoch {i}: Loss = {mean_squared_error(self.output, y)}")
+                print(f"Epoch {i}: Loss = {self.mean_squared_error(self.output, y)}")
                 
-        print(f"Training done. Total Epochs: {epochs} \n Final Loss = {mean_squared_error(self.output, y)}")
+        print(f"Training done. Total Epochs: {epochs} \n Final Loss = {self.mean_squared_error(self.output, y)}")
     
     def test_data(self): #find the accuracy
+        if self.test_X.all() == None:
+            print('ERROR: Dataset not found')
+            return 0
         correct = 0
         X = self.test_X
         for i in range(1000): 
             self.forward(X[i])
-            result = self.reform(self.output[0])
+            result = self.reform(self.output)
             if result == self.test_y[i]: #find if test result is correct or not
                 correct += 1
             
             print(f"Test case #{i+1}: Predict output = {result}, Actual output = {self.test_y[i]} ")
             print(f'Accuracy: {round(((correct/( i+1)) * 100), 2)}%')
 
+    def save_model(self, filename):
+        file = str(filename) 
+        if not filename: 
+            print('ERROR: Please provide filename.')
+        elif not file.endswith('.csv'):
+            print("ERROR: File must be CSV file.")
+        with open(filename, 'w', newline='') as csvf:
+            writer = csv.writer(csvf)
+            #save layer 1 model
+            writer.writerow(['Layer 1 Weight'])
+            writer.writerows(self.weight_1)
+            writer.writerow(['Layer 1 Bias'])
+            writer.writerows(self.bias_1)
+            #save layer 2 model
+            writer.writerow(['Layer 2 Weight'])
+            writer.writerows(self.weight_2)
+            writer.writerow(['Layer 2 Bias'])
+            writer.writerows(self.bias_2)
+            #save layer 3 model
+            writer.writerow(['Layer 3 Weight'])
+            writer.writerows(self.weight_3)
+            writer.writerow(['Layer 3 Bias'])
+            writer.writerows(self.bias_3)
+        print('Trained model saved.')
+        self.load_model(filename)
+
+    def load_model(self, filename):
+            print('Loading trained model...')
+            self.weight_1 = []
+            self.weight_2 = []
+            self.weight_3 = []
+            self.bias_1 = None
+            self.bias_2 = None
+            self.bias_3 = None
+            #try:
+            with open(filename, 'r') as csvf:
+                reader = csv.reader(csvf)
+                layer = None
+                for row in reader:
+                    if len(row) > 0 and row[0].startswith('Layer'):
+                        layer = row[0]
+                    elif layer == 'Layer 1 Weight':
+                        self.weight_1.append(np.array([float(val) for val in row]))
+                    elif layer == 'Layer 1 Bias':
+                        self.bias_1 = np.array([float(val) for val in row])
+                    elif layer == 'Layer 2 Weight':
+                        self.weight_2.append(np.array([float(val) for val in row]))
+                    elif layer == 'Layer 2 Bias':
+                        self.bias_2 = np.array([float(val) for val in row])
+                    elif layer == 'Layer 3 Weight':
+                        self.weight_3.append(np.array([float(val) for val in row]))
+                    elif layer == 'Layer 3 Bias':
+                        self.bias_3 = np.array([float(val) for val in row])
+                self.weight_1 = np.array(self.weight_1)
+                self.weight_3 = np.array(self.weight_3)
+            print("Trained model loaded.")
+            #except:
+             #   print('ERROR: File not found')
+              #  return 0
     def showimg(self, X):  
         pyplot.imshow(X, cmap=pyplot.get_cmap('gray'))
         pyplot.show()    
        
     def predict(self, num): #find result and show image used
         self.forward(self.test_X[num])
-        result = self.reform(self.output[0])
-        print(f"Image shows: {result}")
-        self.showimg(self.imgdata[num])
-
-
-if __name__ == '__main__':
-    (train_X, train_y),(test_X, test_y) = mnist.load_data()
-    ml = Convulational_neural_network(train_X, train_y, test_X, test_y, 0.01)
-    ml.train(1)
-    ml.test_data()
-    while True:
-        number = int(input("Enter number from 1-10000:"))
-        ml.predict(number)
+        result = self.reform(self.output)
+        return result
+    
