3fdee019993613cbf3060acc5c0ed3a99999d362
hecka_Jm7 <77819603+Jm7hecka@users.noreply.github.com>
2023-10-09 00:56:55 +0100

    Update neural_network.py
    output layer included into layer_output

--- hand write digital recognising/new_ver/neural_network.py
+++ hand write digital recognising/new_ver/neural_network.py
@@ -1,8 +1,10 @@
+import numpy as np
+import time
 from math import sqrt
 import numpy as np
 from keras.models import save_model
 import csv 
-from matplotlib import pyplot 
+import threading
 import Activation_function as af
 import ProgressBar
 class MLP():
@@ -91,23 +93,23 @@ class MLP():
         for i in range(len(self.weight)-1): 
             self.layer_output.append(af.Activation_function(np.dot(self.layer_output[-1], self.weight[i]) + self.bias[i], 'relu'))
             
-        self.output  = af.Activation_function(np.dot(self.layer_output[-1], self.weight[-1]) + self.bias[-1], 'sigmoid')
+        self.layer_output.append(af.Activation_function(np.dot(self.layer_output[-1], self.weight[-1]) + self.bias[-1], 'sigmoid'))
 
-        return self.output
+        return self.layer_output
     
-    def backpropagation(self, layer_output, y_pred, y_actual): #changing weight and bias value base on error rate 
+    def backpropagation(self, layer_output, y_actual): #changing weight and bias value base on error rate 
         x = layer_output[0].reshape(len(layer_output[0]), -1)
         gradient_descent = None
         error = None
         gradient_w = [np.zeros_like(w) for w in self.weight]
         gradient_b = [np.zeros_like(b) for b in self.bias]
         self.batch_gradient_descent = [None, None]
-        error = y_actual - y_pred
-        gradient_descent = error * af.Activation_function(y_pred, 'sigmoid', True)
-        gradient_w[-1] = np.sum([np.dot(self.layer_output[-1][j].reshape(1, -1).T, gradient_descent[j].reshape(1, -1)) for j in range(self.batch_size)], axis=0)
+        error = y_actual - layer_output[-1]
+        gradient_descent = error * af.Activation_function(layer_output[-1], 'sigmoid', True)
+        gradient_w[-1] = np.sum([np.dot(self.layer_output[-2][j].reshape(1, -1).T, gradient_descent[j].reshape(1, -1)) for j in range(self.batch_size)], axis=0)
         gradient_b[-1] = np.sum((gradient_descent[j] for j in range(self.batch_size)), axis=0)
         
-        for i in range(len(self.layer_output)-1, 0, -1):
+        for i in range(len(self.layer_output)-2, 0, -1):
             error = gradient_descent.dot(self.weight[i].T)
             gradient_descent = error * af.Activation_function(self.layer_output[i], 'relu', True)
             gradient_w[i-1] = np.sum([self.layer_output[i-1][j].reshape(1, -1).T.dot(gradient_descent[j].reshape(1, -1)) for j in range(self.batch_size)], axis=0)
@@ -115,7 +117,7 @@ class MLP():
        
         self.batch_gradient_descent = [gradient_w, gradient_b]
         
-        for i in range(len(self.batch_gradient_descent[0])-1, -1, -1):
+        for i in range(len(self.batch_gradient_descent[0])):
             self.weight[i] += self.batch_gradient_descent[0][i] * self.learning_rate
             self.bias[i] += self.batch_gradient_descent[1][i] * self.learning_rate
         
@@ -148,13 +150,13 @@ class MLP():
             s = p.run()
             for j in range(len(self.mini_batch[0])):
                 self.output = self.forward(self.mini_batch[0][j])
-                gd = self.backpropagation(self.layer_output, self.output, self.mini_batch[1][j])
-                accuracy = self.accuracy(self.Reform(self.output[-1], self.label_classes), self.Reform(self.mini_batch[1][j][-1], self.label_classes), j)
-                loss = self.mean_squared_error(self.output[-1], self.mini_batch[1][j][-1])
+                gd = self.backpropagation(self.output, self.mini_batch[1][j])
+                accuracy = self.accuracy(self.Reform(self.output[-1][-1], self.label_classes), self.Reform(self.mini_batch[1][j][-1], self.label_classes), j)
+                loss = self.mean_squared_error(self.output[-1][-1], self.mini_batch[1][j][-1])
                 s(loss, accuracy)
             
-        print(f"Training done. Total Epochs: {epochs} \n Final Loss = {self.mean_squared_error(self.output, self.mini_batch[1][j])}")
-        print(f"Layer: {layer_neuron}, Learning rate:{learning_rate}, Batch size: {batch_size}, Total Batch: {len(self.mini_batch[0])}")
+        print(f"Training done. Total Epochs: {epochs} \n Final Loss = {self.mean_squared_error(self.output[-1][-1], self.mini_batch[1][j])}")
+        print(f"Layers: {layer_neuron}, Learning rate:{learning_rate}, Batch size: {batch_size}, Total Batch: {len(self.mini_batch[0])}")
     
     def TestData(self): #find the accuracy
         if self.test_X.all() == None:
@@ -162,7 +164,7 @@ class MLP():
         correct = 0
         X = self.test_X
         for i in range(1000): 
-            output = self.forward(X[i])
+            output = self.forward(X[i])[-1]
             if self.isclasses:
                 result = self.Reform(output, self.label_classes)
             else:
@@ -173,6 +175,71 @@ class MLP():
             print(f"Test case #{i+1}: Predict output = {result}, Actual output = {self.test_y[i]} ")
             print(f'Accuracy: {round(((correct/( i+1)) * 100), 2)}%')
 
+    def save_model(self, filename):
+        file = str(filename) 
+        if not filename: 
+            print('ERROR: Please provide filename.')
+        elif not file.endswith('.csv'):
+            print("ERROR: File must be CSV file.")
+        with open(filename, 'w', newline='') as csvf:
+            writer = csv.writer(csvf)
+            #save layer 1 model
+            writer.writerow(['Layer 1 Weight'])
+            writer.writerows(self.weight_1)
+            writer.writerow(['Layer 1 Bias'])
+            writer.writerows(self.bias_1)
+            #save layer 2 model
+            writer.writerow(['Layer 2 Weight'])
+            writer.writerows(self.weight_2)
+            writer.writerow(['Layer 2 Bias'])
+            writer.writerows(self.bias_2)
+            if self.twolayers:
+                #save layer 3 model
+                writer.writerow(['Layer 3 Weight'])
+                writer.writerows(self.weight_3)
+                writer.writerow(['Layer 3 Bias'])
+                writer.writerows(self.bias_3)
+            
+        print('Trained model saved.')
+        self.load_model(filename)
+
+    def load_model(self, filename):
+            print('Loading trained model...')
+            self.weight_1 = []
+            self.weight_2 = []
+            self.weight_3 = []
+            self.bias_1 = None
+            self.bias_2 = None
+            self.bias_3 = None
+            self.twolayers = False
+            #try:
+            with open(filename, 'r') as csvf:
+                reader = csv.reader(csvf)
+                layer = None
+                for row in reader:
+                    if len(row) > 0 and row[0].startswith('Layer'):
+                        layer = row[0]
+                    elif layer == 'Layer 1 Weight':
+                        self.weight_1.append(np.array([float(val) for val in row]))
+                    elif layer == 'Layer 1 Bias':
+                        self.bias_1 = np.array([float(val) for val in row])
+                    elif layer == 'Layer 2 Weight':
+                        self.weight_2.append(np.array([float(val) for val in row]))
+                    elif layer == 'Layer 2 Bias':
+                        self.bias_2 = np.array([float(val) for val in row])
+                    elif layer == 'Layer 3 Weight':
+                        self.weight_3.append(np.array([float(val) for val in row]))
+                        self.twolayers = True
+                    elif layer == 'Layer 3 Bias':
+                        self.bias_3 = np.array([float(val) for val in row])
+                if self.twolayers:
+                    self.weight_3 = np.array(self.weight_3)
+                self.weight_1 = np.array(self.weight_1)
+                self.weight_2 = np.array(self.weight_2)
+            print("Trained model loaded.")
+            #except:
+             #   print('ERROR: File not found')
+              #  return 0
        
     def predict(self, image): #find result and show image used
         self.forward()
